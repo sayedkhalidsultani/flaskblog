bf91e3e41c31c872ddbf3929f403a94956990c89
SayedKhalidSultani <sayedkhalidsultani@hotmail.com>
2022-08-13 12:34:54 +0430

    modified

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = 'f567ce99ed2b92bfad887060859afe65'
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
